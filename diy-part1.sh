50920d5ab2b62a5e0f12c671d718ac2d4e50c38c
maixuanhao <46432287+maixuanhao@users.noreply.github.com>
2026-07-19 20:45:37 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,1 +1,7 @@
 
+#!/bin/bash
+# Thêm nguồn ứng dụng PassWall ngoài vào Feeds hệ thống
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+
+# Thêm nguồn ứng dụng OpenClash ngoài vào Feeds hệ thống
+echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
